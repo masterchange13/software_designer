c1c074430b0e8f294bf3104ef3e50cb6ee8b090c
masterchange <2042313869@qq.com>
2024-04-03 07:26:47 +0800

    prefix code

--- src/test/java/org/MaoGuangZhi206/NumberTest.java
+++ src/test/java/org/MaoGuangZhi206/NumberTest.java
@@ -25,6 +25,7 @@ class NumberTest {
 
     Number number;
     private static Set<Integer> MonthWithThirtyDay;
+
     static {
         MonthWithThirtyDay = Stream.of(4, 6, 9, 11).collect(Collectors.toSet());
     }
@@ -36,21 +37,55 @@ class NumberTest {
     }
 
     @AfterEach
-    void tearDown(){
+    void tearDown() {
 
     }
 
     @ParameterizedTest
-    //第一个代表期望值；后面的代表你要测试的方法的参数，有几个就写几个
-    //基于语句覆盖测试用例
+    // 第一个代表期望值；后面的代表你要测试的方法的参数，有几个就写几个
+    // 基于语句覆盖测试用例
     @CsvSource({"'20030611', 2003, 06, 10"})
-    void nextDateCaseDay(String expected,String year,String month,String day) {
-        assertEquals(expected,number.NextDateCaseDay(year, month, day));
+    void nextDateCaseDay(String expected, String year, String month, String day) {
+        assertEquals(expected, number.NextDateCaseDay(year, month, day));
     }
 
     @ParameterizedTest
     @CsvFileSource(files = "src/test/resources/number.csv")
-    void nextDateCaseDay2(String expected,String year,String month,String day) {
+    void nextDateCaseDay2(String expected, String year, String month, String day) {
         assertEquals(expected, number.NextDateCaseDay(year, month, day));
     }
+
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/blackBox.csv")
+    void testBlackBox(String expected, String year, String month, String day) {
+        assertEquals(expected, number.NextDateCaseDay(year, month, day));
+    }
+
+    //     黑盒测试，检查日期是否可以检查错误
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/blackBoxData.csv")
+    void testBlackBoxData(String expected, String year, String month, String day) {
+        assertEquals(expected, number.NextDateCaseDay(year, month, day));
+    }
+
+    //     白盒测试
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/writeBoxData.csv")
+    void testWriteBoxData(String expected, String year, String month, String day) {
+        assertEquals(expected, number.NextDateCaseDay(year, month, day));
+    }
+
+    @ParameterizedTest
+    @CsvSource({"'20021030', 2002, 10, 29"})
+    void testBirthDay(String expected, String year, String month, String day){
+        assertEquals(expected, number.NextDateCaseDay(year, month, day));
+    }
+
+//     等价类测试
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/equal.csv")
+    void testEqual(String expected, String year, String month, String day) {
+        assertEquals(expected, number.NextDateCaseDay(year, month, day));
+    }
+
 }
